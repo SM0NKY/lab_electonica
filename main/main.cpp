0203783108fda1b6836b940016e7d62b12da1510
SM0NKY <smonky123.9.ki@homail.com>
2026-05-26 17:19:05 -0700

    remplazo y correcciones

--- main/main.cpp
+++ main/main.cpp
@@ -10,6 +10,7 @@
 
 //Pin motor PWM
 #define MOTOR_PWM1 GPIO_NUM_26
+#define MOTOR_PWM2 GPIO_NUM_27
 //Pin del botón
 #define BUTTON GPIO_NUM_32
 
@@ -18,23 +19,23 @@ static const char *TAG = "Button";
 extern "C" void app_main(void) 
 {
     Driver1 motors(MOTOR_PWM1); //Inicializar la clase del motor con el pin PWM1
+    Driver1 motors2(MOTOR_PWM2); //Inicializar la clase del motor con el pin PWM2
     Button button(BUTTON); //Inicializar la clase del botón con el pin del botón
     while (1) {
-        int button_state = gpio_get_level(BUTTON);
-        ESP_LOGI(TAG, "Estado del botón: %d", button_state);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Pequeña demora para evitar lecturas rápidas del botón
 
-        if (button_state == 0) {
-            ESP_LOGI(TAG, "Botón presionado, cambiando estado del motor");
-            button.button_state_flip(); // Cambia el estado del botón
-            if (button.button_state) {
-                ESP_LOGI(TAG, "Motor a un angulo de 90 grados");
-                motors.motor_angular_movement(90.0);
-            } else {
-                ESP_LOGI(TAG, "Motor a un angulo de 0 grados");
-                motors.motor_angular_movement(0.0);
-            }
-            vTaskDelay(pdMS_TO_TICKS(200));
-        }
+        motors.motor_angular_movement(0.0); // Inicializar el motor en 0 grados
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Pequeña demora para evitar lecturas rápidas del botón
+
+        motors.motor_angular_movement(90.0); // Mover el motor a 90 grados
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Pequeña demora para evitar lecturas rápidas del botón
         
+        motors2.motor_angular_movement(0.0); // Inicializar el motor en 0 grados
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Pequeña demora para evitar lecturas rápidas del botón
+        motors2.motor_angular_movement(180.0); // Mover el motor a 180 grados
+
     }
+        
 }
